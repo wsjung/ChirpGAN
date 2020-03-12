bd77bbdb288218b14fd6fd4b8fe6063e80cdccc8
Woo <wooseokjung0826@gmail.com>
2020-03-11 19:13:30 -0700

    wrote bucket fill algorithm and found that a threshold=10 is optimal

--- scalogram/floodfill.py
+++ scalogram/floodfill.py
@@ -9,57 +9,117 @@ class Floodfill:
 	def __init__(self, source, name = None):
 		if type(source) == str:
 			if source.lower().endswith('.png'):
-				png_data = np.array(Image.open(source))
+				img = Image.open(source)
+				tmp = np.array(img)
 
-				plt.figure(figsize=(50,50))
-				plt.imshow(png_data[:,15000:15250], cmap='inferno')
+				# display image
+				f = plt.figure(figsize=(10,10))
+				plt.imshow(tmp[:,12000:20000], cmap='inferno')
 				plt.show()
+				f.savefig('./z_floodfill_testruns/orig.png', bbox_inches='tight')
+				img.close()
+				plt.close(f)
 
-				flooded_png = Floodfill.flood_iterative(png_data[:,15000:15250], (0,0), 255, 0, 15)
 
-				plt.figure(figsize=(50,50))
-				plt.imshow(png_data[:15000:15250], cmap='inferno')
-				plt.show()
+				
+				# bucket fill threshold range
+				for threshold in range(10,11):
+					print('### Testing threshold = %d ###' % threshold, flush=True)
 
-	
-	def within_threshold(x, target_color, threshold):
+					img = Image.open(source)
+					png_data = np.array(img)
+
+					flooded_png = Floodfill.flood_iterative(png_data[:,12000:20000], xy=(50,50), target_color=255, replacement_color=0, threshold=threshold)
+					
+
+					#print(flooded_png)
+					#print(flooded_png.dtype)
+
+
+					# save bucket filled image
+					fig = plt.figure(figsize=(10,10))
+					plt.imshow(flooded_png, cmap='inferno')
+					print('saving file..', end='', flush=True)
+					fig.savefig('./z_floodfill_testruns/8d_test_threshold_%d.png' % threshold, bbox_inches='tight', format='png')
+					print('.', flush=True)
+
+
+					# close figure and image
+					plt.close(fig)
+					img.close()
+
+
+	def within_threshold(x, target, threshold):
 		"""
 		within_threshold checks whether cell is within the target_color 
 		and the specified threshold bands
 		"""
-		return x >= (target_color - threshold) or x <= (target_color + threshold)
+		return x > (target - threshold) and x < (target + threshold)
+
+	def within_image(x, y, shape):
+		return x >= 0 and x < shape[0] and y >= 0 and y < shape[1]
+
 
 	def flood_iterative(image, xy, target_color, replacement_color, threshold):
+		"""
+		Args:
+			image (np.array): image 
+			xy (tuple (x,y)): starting location 
+			target_color (int): the color we want replaced
+			replacement_color (int): the color we want replaced into
+			threshold (int): the threshold for classifying a color as target_color
+		"""
 		x = xy[0]
 		y = xy[1]
 
-		print(image.shape)
 
-		if target_color == replacement_color:
+		print('image shape: ', image.shape)
+
+		if target_color == replacement_color:	# already the specified replacement_color 
 			return
-		elif not Floodfill.within_threshold(image[x,y], target_color, threshold):
+		elif not Floodfill.within_threshold(image[x,y], target_color, threshold):	# not the target color we want
 			return	
 		else:
-			image[x,y] = replacement_color
-			q = Queue()
-			q.put(xy)
-			while not q.empty():
-				xy = q.get()
-				print(xy)
-				x = xy[0]
-				y = xy[1]
-				if x+1 < image.shape[0] and Floodfill.within_threshold(image[x+1,y], target_color, threshold):
-					image[x+1,y] = replacement_color
-					q.put([x+1,y])
-				if x-1 >= 0 and Floodfill.within_threshold(image[x-1,y], target_color, threshold):
-					image[x-1,y] = replacement_color
-					q.put([x-1,y])
-				if y-1 >= 0 and Floodfill.within_threshold(image[x,y-1], target_color, threshold):
-					image[x,y-1] = replacement_color
-					q.put([x,y-1])
-				if y+1 < image.shape[0] and Floodfill.within_threshold(image[x,y+1], target_color, threshold):
-					image[x,y+1] = replacement_color
-					q.put([x,y+1])
+			init_target_color = image[x,y]	# set initial target color
+			print('init_target_color: %d' % init_target_color)
+
+			sq = SetQueue()	# queue for future locations
+			sq.put(xy)
+
+			
+			#i = 0
+
+
+			while not sq.empty():
+				#i += 1
+
+				#if i%10000==0:
+				#	plt.figure()
+				#	plt.imshow(image, cmap='inferno')
+				#	plt.show()
+
+
+
+
+				loc = sq.get()	# get next location
+				x = loc[0]
+				y = loc[1]
+				
+				if Floodfill.within_image(x, y, image.shape):
+					if Floodfill.within_threshold(image[x,y], init_target_color, threshold):	# replace color if within target threshold
+						image[x,y] = replacement_color
+				
+					# add neighbors to the queue
+					sq.put((x+1,y))
+					sq.put((x+1,y+1))
+					sq.put((x+1,y-1))
+					sq.put((x-1,y))
+					sq.put((x-1,y+1))
+					sq.put((x-1,y-1))
+					sq.put((x,y+1))
+					sq.put((x,y-1))
+
+		return image
 
 
 	def flood(image, xy, target_color, replacement_color, threshold):
@@ -95,3 +155,29 @@ class Floodfill:
 
 	def write_to_png(image):
 		print('writing to PNG')
+
+class SetQueue:
+
+	def put(self, item):
+		if item not in self.setqueue_set:
+			self.setqueue_set.add(item)
+			self.setqueue_queue.put(item)
+			self.size += 1
+
+	def get(self):
+		item = self.setqueue_queue.get()
+		#self.setqueue_set.remove(item)
+		self.size -= 1
+		return item
+
+	def empty(self):
+		return self.size==0
+
+	def __init__(self):
+		self.setqueue_set = set()
+		self.setqueue_queue = Queue()
+		self.size = 0
+
+	
+
+
