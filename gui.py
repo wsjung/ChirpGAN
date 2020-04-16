74e14183fdcf358296dd17c41f597c1760c1a737
dalbosta <derek.albosta@gmail.com>
2020-04-16 04:16:13 +0200

    finished pipline from sever

--- gui.py
+++ gui.py
@@ -1,39 +1,89 @@
 import PySimpleGUI as sg
+import os
+import tempfile
+import gzip
+import shutil
+import glob
+
+from scalogram.floodfill import Floodfill
+from scalogram.scalogram import Scalogram
+from scalogram import data_process_wav as pwave
+
 
 #sg.theme('DarkAmber')	# color
 
 # basic window
 
 main_layout = [
-	[sg.Text('Main Menu')],
-	[sg.Button('Load Data')],
-	[sg.Exit()]
+[sg.Text('Main Menu')],
+[sg.Button('Load Data')],
+[sg.Exit()]
 ]
 
+#TODO: allow ability to select and load multiple files
 load_data_layout = [
-	[sg.Text('Bird vocalization files to load: ')],
-	[sg.Input(key='_FILES_'), sg.FilesBrowse()],
-	[sg.OK(), sg.Cancel()]
+[sg.Text('Folder of bird vocalization files to load: ')],
+[sg.Input(key='_FILES_'), sg.FolderBrowse()], 
+[sg.OK(), sg.Cancel()]
 ]
 
 
-load_data_popup = sg.Window('Select audio file(s)').Layout(load_data_layout)
+load_data_popup = sg.Window('Select folder containing audio files').Layout(load_data_layout)
 main_window = sg.Window('Main Menu').Layout(main_layout)
 
+#main window
 while True:
 	event, values = main_window.read()
 	print('event: %s\nvalues: %s' % (event, values))
 	if event in ('Load Data'):
+		#pop up load window
 		while True:
 			ld_event, ld_values = load_data_popup.read()
 			#print('ld_event: %s\nld_values: %s' % (ld_event, ld_values))
 			if ld_event in ('OK'):
+				
+				
+
+
+				wav_dir = ld_values['_FILES_'].split(';')[0]
+
+				print('### SELECTED FOLDER ###')
+				print(ld_values['_FILES_'].split(';')[0])
+
 				print('### SELECTED FILES ###')
-				print(ld_values['_FILES_'].split(';'))
-			if ld_event in (None, 'Cancel'):
-				load_data_popup.close()
-				break
-	if event in (None, 'Exit'):
-		break
+				for file_name in os.listdir(wav_dir):
+					if file_name.endswith(".wav"):
+						print(file_name)
+
+
+
+				#create file storing wav file transformations
+				if not os.path.exists('./png_scalogram'):
+					os.mkdir('./png_scalogram')
+
+				path = './png_scalogram'
+
+
+
+
+				###########################################################
+				### PASS FILES TO DATA PIPELINE                         ###
+				###########################################################
+
+				
+
+
+
+				if ld_event in (None, 'Cancel'):
+					load_data_popup.close()
+					break
+					if event in (None, 'Exit'):
+						break
+
+
+#TODO: allow ability to select and load multiple files
+#TODO: 
+
+
 
 main_window.close()
