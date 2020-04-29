633841df521c74354b818a09fc2f4e45bede6e2a
Woo Jung <wooseokjung0826@gmail.com>
2020-04-28 17:08:01 -0700

    added feature to make sure selected folder only contains .wav files

--- gui.py
+++ gui.py
@@ -22,14 +22,24 @@ main_layout = [
 ]
 
 #TODO: allow ability to select and load multiple files
-load_data_layout = [
-[sg.Text('Folder of bird vocalization files to load: ')],
-[sg.Input(key='_FILES_'), sg.FolderBrowse()], 
-[sg.OK(), sg.Cancel()]
-]
 
 
-load_data_popup = sg.Window('Select folder containing audio files').Layout(load_data_layout)
+def create_load_data_popup(warning=False):
+	warn_text = [sg.Text(text='  *Make sure folder only contains .wav files.', text_color='#d9534f')]
+
+	load_data_layout = [
+		[sg.Text('Folder of bird vocalization files to load: ')],
+		[sg.Input(key='_FILES_'), sg.FolderBrowse()], 
+		[sg.OK(), sg.Cancel()]
+	]
+
+	if warning:
+		load_data_layout.insert(1, warn_text)
+
+	return sg.Window('Select folder containing audio files').Layout(load_data_layout)
+
+
+load_data_popup = create_load_data_popup()
 main_window = sg.Window('Main Menu').Layout(main_layout)
 
 #main window
@@ -43,207 +53,225 @@ while True:
 
 	if event in ('Load Data'):
 		#pop up load window
+
+		# boolean for showing warning message in load data layout
+		wav_only_warning = False
+
 		while True:
 
-
+			load_data_popup = create_load_data_popup(wav_only_warning)
 			ld_event, ld_values = load_data_popup.read()
 			#print('ld_event: %s\nld_values: %s' % (ld_event, ld_values))
+
 			if ld_event == 'Cancel':
 				load_data_popup.close()
 
-
 			if ld_event in ('OK'):
 				
-				
+				# close load_data_popup
+				load_data_popup.close()
 
 
 				wave_dir = ld_values['_FILES_'].split(';')[0]
 
 				print('### SELECTED FOLDER ###')
-				print(ld_values['_FILES_'].split(';')[0])
+				print(wave_dir)
 
 				print('### SELECTED FILES ###')
 				for file_name in os.listdir(wave_dir):
 					if file_name.endswith(".wav"):
 						print(file_name)
 
+				print('hello')
+				
+				# check for non .wav files in selected folder
+				total_num_files = len(os.listdir(wave_dir))
+				total_num_wavs = len(glob.glob1(wave_dir, '*.wav'))
+				# if non .wav files exist, warn user and return to load_data window
+				if total_num_files == total_num_wavs:
+					wav_only_warning = False
 
 
-				#create file storing wav file transformations
-				if not os.path.exists('./png_scalogram'):
-					os.mkdir('./png_scalogram')
+					#create file storing wav file transformations
+					if not os.path.exists('./png_scalogram'):
+						os.mkdir('./png_scalogram')
 
-				if not os.path.exists('./wav_transform'):
-					os.mkdir('./wav_transform')
+					if not os.path.exists('./wav_transform'):
+						os.mkdir('./wav_transform')
 
-				png_sav_dir = './png_scalogram'
-				wave_sav_dir = './wav_transform'
-
-
-
-
-				###########################################################
-				### PASS FILES TO DATA PIPELINE                         ###
-				###########################################################
-
-				print('SPLITTING\n')
-
-				org_wav = os.stat(wave_dir + "/test.wav")
-				print(f'File size in Bytes is {org_wav.st_size}')
-
-
-				start = time.time()
-				#TODO: add option to split wav files from GUI
-				split_wav = True
-				if (split_wav):
-					WavPipeline.split(wave_dir, wave_sav_dir)
-				stop = time.time()
-
-				time_split= stop-start
+					png_sav_dir = './png_scalogram'
+					wave_sav_dir = './wav_transform'
 
 
 
-				listwavs = os.listdir(wave_sav_dir)
-				totalwavs = len(glob.glob1(wave_sav_dir, '*.wav'))
-				i=1
+
+					###########################################################
+					### PASS FILES TO DATA PIPELINE                         ###
+					###########################################################
+
+					print('SPLITTING\n')
+
+					org_wav = os.stat(wave_dir + "/test.wav")
+					print(f'File size in Bytes is {org_wav.st_size}')
+
+
+					start = time.time()
+					#TODO: add option to split wav files from GUI
+					split_wav = True
+					if (split_wav):
+						WavPipeline.split(wave_dir, wave_sav_dir)
+					stop = time.time()
+
+					time_split= stop-start
 
 
 
-				size_wav = 0
-				size_scl = 0
-				size_png = 0
+					listwavs = os.listdir(wave_sav_dir)
+					totalwavs = len(glob.glob1(wave_sav_dir, '*.wav'))
+					i=1
 
 
-				time_wavToScl = 0
-				time_sclToPng = 0
-				time_flood = 0
 
-				total_files = 0
-
-				try:
-					for splitwav in listwavs:
-						total_files += 1
-				except:
-					print('################## CANNOT FIND FILES ################')
-					exit(-1)
-
-				#define progress bar window
-				pg_layout = [[sg.Text('Processing files')],
-							 [sg.ProgressBar(total_files, orientation='h', size=(20,20), key='progressbar_f')],
-							 [sg.Text('Files processed:'), sg.Text(size=(15,1), key='file_c')],
-							 [sg.ProgressBar(total_files*3, orientation='h', size=(20,20), key='progressbar_p')],
-							 [sg.Text('Number of processes:'), sg.Text(size=(15,1), key='proc_c')],
-							 [sg.Cancel()]]
-
-				pg_window = sg.Window('File Processing').Layout(pg_layout)
-				progress_bar = pg_window['progressbar_f']
-				process_count = pg_window['progressbar_p']
-
-				pg_window.read(timeout=10)
-
-				process_count.UpdateBar(0) 
-				progress_bar.UpdateBar(0) 
-
-				pg_window['file_c'].update('(0/%d)' % total_files)
-				pg_window['proc_c'].update('(0/%d)' % (total_files*3))
-
-				pg_window.read(timeout=10)
+					size_wav = 0
+					size_scl = 0
+					size_png = 0
 
 
-				try:
-					for splitwav in listwavs:
-						print("TEST")
-						print(splitwav)
-						if splitwav.endswith('.wav'):
+					time_wavToScl = 0
+					time_sclToPng = 0
+					time_flood = 0
 
-							### report progress
-							print('(%d/%d)' % (i, totalwavs))
-							#track process
-							p = ((i-1)*3)
+					total_files = 0
 
-							wavname = os.path.join(wave_sav_dir, splitwav)              # .wav file with full path
-							fname = splitwav[:splitwav.rfind('.')]                      # remove .wav extension
-							scalname = os.path.join(png_sav_dir , '%s.scal' % fname)    # .scal file with full path
-							scalgzname = scalname + '.gz'                               # .scal.gz file
-							mp3name = os.path.join(wave_sav_dir, fname + '.mp3')        # .mp3 name
+					try:
+						for splitwav in listwavs:
+							total_files += 1
+					except:
+						print('################## CANNOT FIND FILES ################')
+						exit(-1)
 
-												
-							print('wavname: %s\nfname: %s\nscalname: %s\nscalgzname: %s\nmp3name: %s' % (wavname, fname, scalname, scalgzname, mp3name)) 
-
-							print('WAV TO SCL\n')
-
-							#wav_s = os.stat(wavname)
-							#size_wav += wav_s.st_size
-
-
-							#start = time.time()
-							WavPipeline.wavToScl(wavname, scalname, scalgzname, mp3name)
-							#stop =  time.time()
-
-							#time_wavToScl += (stop-start)  
-
-							process_count.UpdateBar(p+1) 
-							pg_window['proc_c'].update('(%d/%d)' % (p+1,(total_files*3)))
-							pg_window.read(timeout=10)
 					
-							print('SCL TO PNG\n')
 
-							#scl_s = os.stat(scalname)
-							#size_scl += scl_s.st_size
-							
-							#start = time.time()
-							WavPipeline.scalToPng(fname,scalname,scalgzname, png_sav_dir )
-							#stop = time.time()
+					#define progress bar window
+					pg_layout = [[sg.Text('Processing files')],
+								[sg.ProgressBar(total_files, orientation='h', size=(20,20), key='progressbar_f')], # show progress in %
+								[sg.Text('Files processed:'), sg.Text(size=(15,1), key='file_c')],
+								[sg.ProgressBar(total_files*3, orientation='h', size=(20,20), key='progressbar_p')], 
+								[sg.Text('Number of processes:'), sg.Text(size=(15,1), key='proc_c')],
+								[sg.Cancel()]]
 
-							#time_sclToPng += (stop-start)
+					pg_window = sg.Window('File Processing').Layout(pg_layout)
+					progress_bar = pg_window['progressbar_f']
+					process_count = pg_window['progressbar_p']
 
-							process_count.UpdateBar(p+2) 
-							pg_window['proc_c'].update('(%d/%d)' % (p+2,(total_files*3)))
-							pg_window.read(timeout=10)
+					pg_window.read(timeout=10)
 
-							print('PNG FLOOD FILL\n')
+					process_count.UpdateBar(0) 
+					progress_bar.UpdateBar(0) 
 
-							pngname = os.path.join(png_sav_dir , '%s.png' % fname)
+					pg_window['file_c'].update('(0/%d)' % total_files)
+					pg_window['proc_c'].update('(0/%d)' % (total_files*3))
 
-							#png_s = os.stat(pngname)
-							#size_png += png_s.st_size
-
-							#start = time.time()
-							WavPipeline.flood_png(fname, png_sav_dir )
-							#stop = time.time()
-
-							process_count.UpdateBar(p+3) 
-							pg_window['proc_c'].update('(%d/%d)' % (p+3,(total_files*3)))
-							pg_window.read(timeout=10)
-
-							#time_flood += (stop-start)
-							
-							
-
-							progress_bar.UpdateBar(i)  
-							pg_window['file_c'].update('(%d/%d)' % (i,total_files))
-							pg_window.read(timeout=10)
-
-							i+=1
-
-				except:
-					print('################## ERROR DURING DATA PROCESSING ################')
-					exit(-1)
+					pg_window.read(timeout=10)
 
 
-				pg_window.close()
+					try:
+						for splitwav in listwavs:
+							print("TEST")
+							print(splitwav)
+							if splitwav.endswith('.wav'):
 
-				# TODO: SEPARATELY CALL EACH FUNCTION IN THE PIPELINE
-				# TODO: UPDATE THE GUI AND THE USER ON THE PROCESS OF EACH FUNCTION CALL
-				# TODO: UPSCALE THE GUI SIZE SO IT IS NOT TIGHTLY FIT TO THE CURRENT GUI ELEMENTS -> MAKE IT MORE USER-FRIENDLY AND NAVIGABLE
+								### report progress
+								print('(%d/%d)' % (i, totalwavs))
+								#track process
+								p = ((i-1)*3)
+
+								wavname = os.path.join(wave_sav_dir, splitwav)              # .wav file with full path
+								fname = splitwav[:splitwav.rfind('.')]                      # remove .wav extension
+								scalname = os.path.join(png_sav_dir , '%s.scal' % fname)    # .scal file with full path
+								scalgzname = scalname + '.gz'                               # .scal.gz file
+								mp3name = os.path.join(wave_sav_dir, fname + '.mp3')        # .mp3 name
+
+													
+								print('wavname: %s\nfname: %s\nscalname: %s\nscalgzname: %s\nmp3name: %s' % (wavname, fname, scalname, scalgzname, mp3name)) 
+
+								print('WAV TO SCL\n')
+
+								#wav_s = os.stat(wavname)
+								#size_wav += wav_s.st_size
+
+
+								#start = time.time()
+								WavPipeline.wavToScl(wavname, scalname, scalgzname, mp3name)
+								#stop =  time.time()
+
+								#time_wavToScl += (stop-start)  
+
+								process_count.UpdateBar(p+1) 
+								pg_window['proc_c'].update('(%d/%d)' % (p+1,(total_files*3)))
+								pg_window.read(timeout=10)
+						
+								print('SCL TO PNG\n')
+
+								#scl_s = os.stat(scalname)
+								#size_scl += scl_s.st_size
+								
+								#start = time.time()
+								WavPipeline.scalToPng(fname,scalname,scalgzname, png_sav_dir )
+								#stop = time.time()
+
+								#time_sclToPng += (stop-start)
+
+								process_count.UpdateBar(p+2) 
+								pg_window['proc_c'].update('(%d/%d)' % (p+2,(total_files*3)))
+								pg_window.read(timeout=10)
+
+								print('PNG FLOOD FILL\n')
+
+								pngname = os.path.join(png_sav_dir , '%s.png' % fname)
+
+								#png_s = os.stat(pngname)
+								#size_png += png_s.st_size
+
+								#start = time.time()
+								WavPipeline.flood_png(fname, png_sav_dir )
+								#stop = time.time()
+
+								process_count.UpdateBar(p+3)
+								pg_window['proc_c'].update('(%d/%d)' % (p+3,(total_files*3)))
+								pg_window.read(timeout=10)
+
+								#time_flood += (stop-start)
+								
+								
+
+								progress_bar.UpdateBar(i)  
+								pg_window['file_c'].update('(%d/%d)' % (i,total_files))
+								pg_window.read(timeout=10)
+
+								i+=1
+
+					except:
+						print('################## ERROR DURING DATA PROCESSING ################')
+						exit(-1)
+
+
+					pg_window.close()
+
+					# TODO: SEPARATELY CALL EACH FUNCTION IN THE PIPELINE
+					# TODO: UPDATE THE GUI AND THE USER ON THE PROCESS OF EACH FUNCTION CALL
+					# TODO: UPSCALE THE GUI SIZE SO IT IS NOT TIGHTLY FIT TO THE CURRENT GUI ELEMENTS -> MAKE IT MORE USER-FRIENDLY AND NAVIGABLE
 
 
 
-				if ld_event in (None, 'Cancel'):
-					load_data_popup.close()
-					break
-					if event in (None, 'Exit'):
+					if ld_event in (None, 'Cancel'):
+						load_data_popup.close()
 						break
+						if event in (None, 'Exit'):
+							break
+				else:
+					print('non .wav files detected')
+					wav_only_warning = True
 
 			if event in (None, 'Exit'):
 				main_window.close()
